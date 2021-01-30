a203272cefd9c45be8d869d255351fbafff4ad12
Vitalij Ryndin <crashmax2015@mail.ru>
2021-01-31 00:35:34 +0800

    Divide

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,35 @@ export class RoflanCalc {
     constructor() { }
 
     sum(x: number, y: number): number {
-        return ' '.repeat(x).concat(' '.repeat(y)).length
+        return this.lg(this.rp(x).concat(this.rp(y)))
     }
 
     minus(x: number, y: number): number {
-        return ' '.repeat(x).substring(' '.repeat(y).length).length
+        return this.lg(this.rp(x).substring(this.lg(this.rp(y))))
+    }
+
+    divide(x: number, y: number): number {
+        return this.chunk(this.rp(x), this.lg(this.rp(y)))
+    }
+
+    private rp(num: number) {
+        return ' '.repeat(num)
+    }
+
+    private lg(str: string | any[]): number {
+        return str.length
+    }
+
+    private chunk(str: string, lg: number): number {
+        let padString: string[] = []
+        let pad = str.padEnd(Math.ceil(this.lg(str) / lg) * lg, ' ')
+
+        for (let i = 0; i < pad.length / lg; i++) {
+            padString.push(pad.substring(i * lg, (i + 1) * lg))
+        }
+
+        // console.log(`Chunk (divide): ${JSON.stringify(padString)}`)
+
+        return this.lg(padString)
     }
 }
--- test/dev.ts
+++ test/dev.ts
@@ -5,4 +5,5 @@ import { RoflanCalc } from '../src/index'
 
     console.log(`Sum: ${calc.sum(6, 4)}`)
     console.log(`Minus: ${calc.minus(10, 4)}`)
+    console.log(`Divide: ${calc.divide(16, 4)}`)
 })()
